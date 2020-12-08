862eafa2a8bcd3f534cf4836bc75bf4d78d6534c
Paul Caskey <pcaskey@internet2.edu>
2020-12-08 17:05:05 -0600

    add basic auth

--- Workbench/mq/container_files/usr-local-bin/initialize-rabbitmq.sh
+++ Workbench/mq/container_files/usr-local-bin/initialize-rabbitmq.sh
@@ -7,5 +7,6 @@ echo "Waiting for the server to start up..."
 rabbitmqctl -t 30 wait $RABBITMQ_PID_FILE
 echo "OK, creating sampleQueue..."
 rabbitmqadmin declare queue name=sampleQueue
+rabbitmqctl change_password guest password
 echo "Done"
 touch $RABBITMQ_INIT_DONE_FILE
