cd9887a09118bda982186a949ae344d5f7e24743
Chris Hubing <chubing@internet2.edu>
2020-12-10 20:03:50 +0000

    update path for gsh for grouper 2.5

--- Workbench/grouper_daemon/container_files/usr-local-bin/startup.sh
+++ Workbench/grouper_daemon/container_files/usr-local-bin/startup.sh
@@ -3,7 +3,7 @@
 if [[ -e /tmp/initialize.gsh ]]
 then
   echo Executing /tmp/initialize.gsh on the first start
-  /opt/grouper/grouper.apiBinary/bin/gsh /tmp/initialize.gsh
+   /opt/grouper/grouperWebapp/WEB-INF/bin/gsh.sh /tmp/initialize.gsh
   rm /tmp/initialize.gsh
 else
   echo Skipping execution of /tmp/initialize.gsh as it is not present
