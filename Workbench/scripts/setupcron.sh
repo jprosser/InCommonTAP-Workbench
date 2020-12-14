36284a842100402bbbffce6a49c5553a5ed71ccd
Paul Caskey <pcaskey@internet2.edu>
2020-12-14 11:26:30 -0600

    bugfix

--- Workbench/scripts/setupcron.sh
+++ Workbench/scripts/setupcron.sh
@@ -3,7 +3,7 @@ CRONFILE=/csp-tap/InCommonTAP-Examples/Workbench/scripts/csp-cron
 
 #build crontab file
 echo "#update CSP container status" > ${CRONFILE}
-echo "*/5 * * * * /csp-tap/InCommonTAP-Examples/Workbench/scripts/update-health-status.sh" >> ${CRONFILE}
+echo "*/3 * * * * /csp-tap/InCommonTAP-Examples/Workbench/scripts/update-health-status.sh" >> ${CRONFILE}
 chmod 644 ${CRONFILE}
 
 #install crontab
