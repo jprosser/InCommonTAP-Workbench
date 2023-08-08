1e22ead7ebbfa157bfe45006c8f1890216ed370c
Paul Caskey <pcaskey@internet2.edu>
2023-08-08 19:51:24 +0000

    update Grouper to 4.4.0

--- Workbench/grouper_data/container_files/mysql/createSQLuser.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'grouper'@'%' IDENTIFIED BY 'password';
-CREATE USER 'grouper'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON * . * TO 'grouper'@'%';
-GRANT ALL PRIVILEGES ON * . * TO 'grouper'@'localhost';
-FLUSH PRIVILEGES;
